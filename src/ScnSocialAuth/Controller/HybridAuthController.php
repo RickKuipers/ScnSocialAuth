a79b13a929bf1421bbdbb7915ca2b3afa5301ab9
Rick Kuipers <rick93@gmail.com>
2013-03-04 17:09:06 +0100

    Redirect to login after cancel

--- src/ScnSocialAuth/Controller/HybridAuthController.php
+++ src/ScnSocialAuth/Controller/HybridAuthController.php
@@ -7,6 +7,10 @@ class HybridAuthController extends AbstractActionController
 {
     public function indexAction()
     {
+        $error = $this->getRequest()->getQuery()->get('error');
+        if (isset($error)) {
+            return $this->_redirect()->toRoute('zfcuser/login');
+        }
         \Hybrid_Endpoint::process();
     }
 }
